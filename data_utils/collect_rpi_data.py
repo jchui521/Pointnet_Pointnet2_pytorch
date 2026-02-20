ed5238f467031bd88393d41389f17225e37e2028
jchui521 <156844155+jchui521@users.noreply.github.com>
2026-02-20 15:39:41 -0500

    mostly figured out rpi custom data pipeline

--- data_utils/collect_rpi_data.py
+++ data_utils/collect_rpi_data.py
@@ -5,18 +5,16 @@ import open3d as o3d
 import glob
 import colorsys
 
-ROOT = "data/rpi_custom_dataset_01"
-
-def generate_color_map(class_list):
-    color_map = {}
-    n = len(class_list)
-    
-    for i, name in enumerate(class_list):
-        hue = i / n
-        rgb = colorsys.hsv_to_rgb(hue, 0.9, 0.9)
-        color_map[name] = tuple(int(c) for c in rgb)
-        
-    return color_map
+ROOT = "data/rpi_custom_dataset_01_raw"
+classes = [x.rstrip() for x in open(os.path.join(ROOT, "classes.txt"))]
+class2labels = {cls: i for i,cls in enumerate(classes)}
+class_colors = []
+n = len(classes)
+for i, name in enumerate(classes):
+    hue = i / n
+    rgb = colorsys.hsv_to_rgb(hue, 0.9, 0.9)
+    class_colors.append([c for c in rgb])
+class_colors = np.array(class_colors)
 
 def collect_points_labels(anno_dir):
     points_list = []
@@ -36,25 +34,20 @@ def collect_points_labels(anno_dir):
     return data
 
 if __name__ == "__main__":
-    anno_dir = os.path.join(ROOT,"scene_1", "Annotations")
-    anno = os.listdir(anno_dir)
-    print(f"Number of Annotations: {len(anno)}")
+    for scene_dir in os.listdir(ROOT):
+        print(f"Processing Scene: {scene_dir}")
+        anno_dir = os.path.join(ROOT,scene_dir, "Annotations")
+        anno = os.listdir(anno_dir)
+        print(f"Number of Annotations: {len(anno)}")
 
-    classes = list(set(map(lambda x: x.rsplit("_", 1)[0], anno)))
-    class2labels = {cls: i for i, cls in enumerate(classes)}
-    label2color = generate_color_map(classes)
-    colors = list( label2color.items() )
+        data = collect_points_labels(anno_dir)
 
-    print(f"Num Classes: {len(classes)}")
+        out_file = os.path.join(ROOT, "scene_1.npy")
 
-    data = collect_points_labels(anno_dir)
-
-    out_file = os.path.join(ROOT, "scene_1.npy")
-
-    np.save(out_file, data)
-    print(f"Saved as: {out_file}")
-    
-    
+        np.save(out_file, data)
+        print(f"Saved as: {out_file}")
+        
+        
 
 
 
