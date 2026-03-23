761c4901b0791e5c48b6250b396bfc04606be7ce
jchui521 <156844155+jchui521@users.noreply.github.com>
2026-03-23 15:24:18 -0400

    Update train_debug.py

--- train_debug.py
+++ train_debug.py
@@ -70,31 +70,31 @@ if __name__ == "__main__":
         worker_init_fn=lambda id: np.random.seed(id),
     )
 
-    weights = torch.Tensor(ds.labelweights).cuda()
+    # weights = torch.Tensor(ds.labelweights).cuda()
 
     classifier = get_model(
         num_classes=NUM_CLASSES,
     ).cuda()
 
-    criterion = get_loss()
-    optimizer = torch.optim.Adam(classifier.parameters(), lr=LR, betas=(0.9, 0.999))
+    # criterion = get_loss()
+    # optimizer = torch.optim.Adam(classifier.parameters(), lr=LR, betas=(0.9, 0.999))
 
     for i in range(EPOCHS):
-        loss_sum = 0.0
+        # loss_sum = 0.0
         for batch_id, (points, target) in tqdm(enumerate(dataloader), total=len(dataloader)):
-            optimizer.zero_grad()
+            # optimizer.zero_grad()
 
             points, target = points.float().cuda(), target.long().cuda()
             points = points.transpose(2, 1)
 
             seg_pred, trans_feat = classifier(points)
-            seg_pred = seg_pred.contiguous().view(-1, NUM_CLASSES)
+            # seg_pred = seg_pred.contiguous().view(-1, NUM_CLASSES)
 
-            target = target.view(-1, 1)[:, 0]
-            loss = criterion(seg_pred, target, trans_feat, weights)       
+            # target = target.view(-1, 1)[:, 0]
+            # loss = criterion(seg_pred, target, trans_feat, weights)       
 
-            loss.backward()
-            optimizer.step()
+            # loss.backward()
+            # optimizer.step()
 
-            loss_sum += loss
-        print(f"Loss: {loss_sum / len(dataloader)}")
+            # loss_sum += loss
+        # print(f"Loss: {loss_sum / len(dataloader)}")
